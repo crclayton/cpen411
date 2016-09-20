7e74e9f01c1e0e8f803a2625cb0095adf3ca5f50
Charles Clayton <y6b9@ssh-linux2.ece.ubc.ca>
2016-09-20 13:54:00 -0700

    Trying not to break anything

--- assn1/sim-safe.c
+++ assn1/sim-safe.c
@@ -66,29 +66,29 @@
 #include "sim.h"
 
 bool isGPR(int R){
-     return R <= 31 && R >= 1;
+	return R <= 31 && R >= 1;
 }
 
 //assumes little endian
 void printBits(size_t const size, void const * const ptr)
 {
-    unsigned char *b = (unsigned char*) ptr;
-    unsigned char byte;
-    int i, j;
+	unsigned char *b = (unsigned char*) ptr;
+	unsigned char byte;
+	int i, j;
 
-    for (i=size-1;i>=0;i--)
-    {
-        for (j=7;j>=0;j--)
-        {
-            byte = (b[i] >> j) & 1;
-            printf("%u", byte);
-        }
-    }
-    puts("");
+	for (i=size-1;i>=0;i--)
+	{
+		for (j=7;j>=0;j--)
+		{
+			byte = (b[i] >> j) & 1;
+			printf("%u", byte);
+		}
+	}
+	puts("");
 }
 
 int count_bits_different(int R1, int R2){
-  return __builtin_popcount(R1 ^ R2);
+	return __builtin_popcount(R1 ^ R2);
 }
 /*
  * This file implements a functional simulator.  This functional simulator is
@@ -118,140 +118,139 @@ static counter_t sim_num_refs = 0;
 static unsigned int max_insts;
 
 /* register simulator-specific options */
-void
-sim_reg_options(struct opt_odb_t *odb)
+void sim_reg_options(struct opt_odb_t *odb)
 {
-  opt_reg_header(odb, 
-"sim-safe: This simulator implements a functional simulator.  This\n"
-"functional simulator is the simplest, most user-friendly simulator in the\n"
-"simplescalar tool set.  Unlike sim-fast, this functional simulator checks\n"
-"for all instruction errors, and the implementation is crafted for clarity\n"
-"rather than speed.\n"
-		 );
+	opt_reg_header(odb, 
+			"sim-safe: This simulator implements a functional simulator.  This\n"
+			"functional simulator is the simplest, most user-friendly simulator in the\n"
+			"simplescalar tool set.  Unlike sim-fast, this functional simulator checks\n"
+			"for all instruction errors, and the implementation is crafted for clarity\n"
+			"rather than speed.\n"
+		      );
 
-  /* instruction limit */
-  opt_reg_uint(odb, "-max:inst", "maximum number of inst's to execute",
-	       &max_insts, /* default */0,
-	       /* print */TRUE, /* format */NULL);
+	/* instruction limit */
+	opt_reg_uint(odb, "-max:inst", "maximum number of inst's to execute",
+			&max_insts, /* default */0,
+			/* print */TRUE, /* format */NULL);
 
 }
 
 /* check simulator-specific option values */
-void
+	void
 sim_check_options(struct opt_odb_t *odb, int argc, char **argv)
 {
-  /* nada */
+	/* nada */
 }
 
 /* register simulator-specific statistics */
-void
+	void
 sim_reg_stats(struct stat_sdb_t *sdb)
 {
-  stat_reg_counter(sdb, "sim_num_cond_branches" /* label for printing */,
-                   "total conditional branches executed" /*description*/,
-                   &g_total_cond_branches /* pointer to the counter */,
-                   0 /* initial value for the counter */, NULL);
+	stat_reg_counter(sdb, "sim_num_cond_branches" /* label for printing */,
+			"total conditional branches executed" /*description*/,
+			&g_total_cond_branches /* pointer to the counter */,
+			0 /* initial value for the counter */, NULL);
 
-  stat_reg_counter(sdb, "sim_num_uncond_branches" /* label for printing */,
-                   "total unconditional branches executed" /*description*/,
-                   &g_total_uncond_branches /* pointer to the counter */,
-                   0 /* initial value for the counter */, NULL);
- 
-  stat_reg_counter(sdb, "sim_num_fcomp_branches" /* label for printing */,
-                   "total floating comp branches executed" /*description*/,
-                   &g_total_fcomp_branches /* pointer to the counter */,
-                   0 /* initial value for the counter */, NULL);
+	stat_reg_counter(sdb, "sim_num_uncond_branches" /* label for printing */,
+			"total unconditional branches executed" /*description*/,
+			&g_total_uncond_branches /* pointer to the counter */,
+			0 /* initial value for the counter */, NULL);
 
-  stat_reg_counter(sdb, "sim_num_fstore_branches" /* label for printing */,
-                   "total store branches executed" /*description*/,
-                   &g_total_fstore_branches /* pointer to the counter */,
-                   0 /* initial value for the counter */, NULL);
+	stat_reg_counter(sdb, "sim_num_fcomp_branches" /* label for printing */,
+			"total floating comp branches executed" /*description*/,
+			&g_total_fcomp_branches /* pointer to the counter */,
+			0 /* initial value for the counter */, NULL);
 
-  stat_reg_counter(sdb, "sim_num_fload_branches" /* label for printing */,
-                   "total load branches executed" /*description*/,
-                   &g_total_fload_branches /* pointer to the counter */,
-                   0 /* initial value for the counter */, NULL);
+	stat_reg_counter(sdb, "sim_num_fstore_branches" /* label for printing */,
+			"total store branches executed" /*description*/,
+			&g_total_fstore_branches /* pointer to the counter */,
+			0 /* initial value for the counter */, NULL);
 
-  stat_reg_counter(sdb, "sim_num_fimm_branches" /* label for printing */,
-                   "total imm branches executed" /*description*/,
-                   &g_total_fimm_branches /* pointer to the counter */,
-                   0 /* initial value for the counter */, NULL);
+	stat_reg_counter(sdb, "sim_num_fload_branches" /* label for printing */,
+			"total load branches executed" /*description*/,
+			&g_total_fload_branches /* pointer to the counter */,
+			0 /* initial value for the counter */, NULL);
 
-  stat_reg_counter(sdb, "sim_num_total_bit_change" /* label for printing */,
-                   "total number of register bits change" /*description*/,
-                   &g_total_register_bit_switch /* pointer to the counter */,
-                   0 /* initial value for the counter */, NULL);
+	stat_reg_counter(sdb, "sim_num_fimm_branches" /* label for printing */,
+			"total imm branches executed" /*description*/,
+			&g_total_fimm_branches /* pointer to the counter */,
+			0 /* initial value for the counter */, NULL);
 
-  stat_reg_counter(sdb, "sim_num_register_change" /* label for printing */,
-                   "total number of operations on registers 1-32" /*description*/,
-                   &g_total_register_operations /* pointer to the counter */,
-                   0 /* initial value for the counter */, NULL);
+	stat_reg_counter(sdb, "sim_num_total_bit_change" /* label for printing */,
+			"total number of register bits change" /*description*/,
+			&g_total_register_bit_switch /* pointer to the counter */,
+			0 /* initial value for the counter */, NULL);
+
+	stat_reg_counter(sdb, "sim_num_register_change" /* label for printing */,
+			"total number of operations on registers 1-32" /*description*/,
+			&g_total_register_operations /* pointer to the counter */,
+			0 /* initial value for the counter */, NULL);
 
 
-  stat_reg_formula(sdb, "sim_cond_branch_freq",
-                   "relative frequency of conditional branches",
-                   "sim_num_cond_branches / sim_num_insn", NULL);
+	stat_reg_formula(sdb, "sim_cond_branch_freq",
+			"relative frequency of conditional branches",
+			"sim_num_cond_branches / sim_num_insn", NULL);
 
-  stat_reg_counter(sdb, "sim_num_insn",
-		   "total number of instructions executed",
-		   &sim_num_insn, sim_num_insn, NULL);
-  stat_reg_counter(sdb, "sim_num_refs",
-		   "total number of loads and stores executed",
-		   &sim_num_refs, 0, NULL);
-  stat_reg_int(sdb, "sim_elapsed_time",
-	       "total simulation time in seconds",
-	       &sim_elapsed_time, 0, NULL);
-  stat_reg_formula(sdb, "sim_inst_rate",
-		   "simulation speed (in insts/sec)",
-		   "sim_num_insn / sim_elapsed_time", NULL);
-  ld_reg_stats(sdb);
-  mem_reg_stats(mem, sdb);
+	stat_reg_counter(sdb, "sim_num_insn",
+			"total number of instructions executed",
+			&sim_num_insn, sim_num_insn, NULL);
+	stat_reg_counter(sdb, "sim_num_refs",
+			"total number of loads and stores executed",
+			&sim_num_refs, 0, NULL);
+	stat_reg_int(sdb, "sim_elapsed_time",
+			"total simulation time in seconds",
+			&sim_elapsed_time, 0, NULL);
+	stat_reg_formula(sdb, "sim_inst_rate",
+			"simulation speed (in insts/sec)",
+			"sim_num_insn / sim_elapsed_time", NULL);
+	ld_reg_stats(sdb);
+	mem_reg_stats(mem, sdb);
 }
 
 /* initialize the simulator */
-void
+	void
 sim_init(void)
 {
-  sim_num_refs = 0;
+	sim_num_refs = 0;
 
-  /* allocate and initialize register file */
-  regs_init(&regs);
+	/* allocate and initialize register file */
+	regs_init(&regs);
 
 
-  /* allocate and initialize memory space */
-  mem = mem_create("mem");
-  mem_init(mem);
+	/* allocate and initialize memory space */
+	mem = mem_create("mem");
+	mem_init(mem);
 }
 
 /* load program into simulated state */
-void
+	void
 sim_load_prog(char *fname,		/* program to load */
-	      int argc, char **argv,	/* program arguments */
-	      char **envp)		/* program environment */
+		int argc, char **argv,	/* program arguments */
+		char **envp)		/* program environment */
 {
-  /* load program text and data, set up environment, memory, and regs */
-  ld_load_prog(fname, argc, argv, envp, &regs, mem, TRUE);
+	/* load program text and data, set up environment, memory, and regs */
+	ld_load_prog(fname, argc, argv, envp, &regs, mem, TRUE);
 }
 
 /* print simulator-specific configuration information */
-void
+	void
 sim_aux_config(FILE *stream)		/* output stream */
 {
-  /* nothing currently */
+	/* nothing currently */
 }
 
 /* dump simulator-specific auxiliary simulator statistics */
-void
+	void
 sim_aux_stats(FILE *stream)		/* output stream */
 {
-   printf("Test"); 
+	printf("Test"); 
 }
 
 /* un-initialize simulator-specific state */
-void
+	void
 sim_uninit(void)
 {
-  /* nada */
+	/* nada */
 }
 
 
@@ -311,25 +310,25 @@ sim_uninit(void)
 
 /* precise architected memory state accessor macros */
 #define READ_BYTE(SRC, FAULT)						\
-  ((FAULT) = md_fault_none, addr = (SRC), MEM_READ_BYTE(mem, addr))
+	((FAULT) = md_fault_none, addr = (SRC), MEM_READ_BYTE(mem, addr))
 #define READ_HALF(SRC, FAULT)						\
-  ((FAULT) = md_fault_none, addr = (SRC), MEM_READ_HALF(mem, addr))
+	((FAULT) = md_fault_none, addr = (SRC), MEM_READ_HALF(mem, addr))
 #define READ_WORD(SRC, FAULT)						\
-  ((FAULT) = md_fault_none, addr = (SRC), MEM_READ_WORD(mem, addr))
+	((FAULT) = md_fault_none, addr = (SRC), MEM_READ_WORD(mem, addr))
 #ifdef HOST_HAS_QWORD
 #define READ_QWORD(SRC, FAULT)						\
-  ((FAULT) = md_fault_none, addr = (SRC), MEM_READ_QWORD(mem, addr))
+	((FAULT) = md_fault_none, addr = (SRC), MEM_READ_QWORD(mem, addr))
 #endif /* HOST_HAS_QWORD */
 
 #define WRITE_BYTE(SRC, DST, FAULT)					\
-  ((FAULT) = md_fault_none, addr = (DST), MEM_WRITE_BYTE(mem, addr, (SRC)))
+	((FAULT) = md_fault_none, addr = (DST), MEM_WRITE_BYTE(mem, addr, (SRC)))
 #define WRITE_HALF(SRC, DST, FAULT)					\
-  ((FAULT) = md_fault_none, addr = (DST), MEM_WRITE_HALF(mem, addr, (SRC)))
+	((FAULT) = md_fault_none, addr = (DST), MEM_WRITE_HALF(mem, addr, (SRC)))
 #define WRITE_WORD(SRC, DST, FAULT)					\
-  ((FAULT) = md_fault_none, addr = (DST), MEM_WRITE_WORD(mem, addr, (SRC)))
+	((FAULT) = md_fault_none, addr = (DST), MEM_WRITE_WORD(mem, addr, (SRC)))
 #ifdef HOST_HAS_QWORD
 #define WRITE_QWORD(SRC, DST, FAULT)					\
-  ((FAULT) = md_fault_none, addr = (DST), MEM_WRITE_QWORD(mem, addr, (SRC)))
+	((FAULT) = md_fault_none, addr = (DST), MEM_WRITE_QWORD(mem, addr, (SRC)))
 #endif /* HOST_HAS_QWORD */
 
 /* system call handler macro */
@@ -353,137 +352,139 @@ sim_uninit(void)
 #define DTMP            (3+32+32)
 
 /* start simulation, program loaded, processor precise state initialized */
-void
+	void
 sim_main(void)
 {
-  md_inst_t inst;
-  register md_addr_t addr;
-  enum md_opcode op;
-  register int is_write;
-  enum md_fault_type fault;
-  int dst_reg;
- 
-  int prv_reg;
-  int new_reg;
+	md_inst_t inst;
+	register md_addr_t addr;
+	enum md_opcode op;
+	register int is_write;
+	enum md_fault_type fault;
+	int dst_reg;
 
-  int bits_diff;
-  
+	int prv_reg;
+	int new_reg;
 
+	int bits_diff;
 
 
 
 
-  fprintf(stderr, "nclude <stdbool.h>sim: ** starting functional simulation **\n");
 
-  /* set up initial default next PC */
-  regs.regs_NPC = regs.regs_PC + sizeof(md_inst_t);
 
-   int ii = 0;
+	fprintf(stderr, "nclude <stdbool.h>sim: ** starting functional simulation **\n");
 
-  while (TRUE)
-    {
-      if(ii > 0) prv_reg = new_reg;
-      ii++;
+	/* set up initial default next PC */
+	regs.regs_NPC = regs.regs_PC + sizeof(md_inst_t);
 
-      /* maintain $r0 semantics */
-      regs.regs_R[MD_REG_ZERO] = 0;
+	int ii = 0;
+
+	while (TRUE)
+	{
+		if(ii > 0) prv_reg = new_reg;
+		ii++;
+
+		/* maintain $r0 semantics */
+		regs.regs_R[MD_REG_ZERO] = 0;
 #ifdef TARGET_ALPHA
-      regs.regs_F.d[MD_REG_ZERO] = 0.0;
+		regs.regs_F.d[MD_REG_ZERO] = 0.0;
 #endif /* TARGET_ALPHA */
 
-      /* get the next instruction to execute */
-      MD_FETCH_INST(inst, mem, regs.regs_PC);
+		/* get the next instruction to execute */
+		MD_FETCH_INST(inst, mem, regs.regs_PC);
 
-      /* keep an instruction count */
-      sim_num_insn++;
+		/* keep an instruction count */
+		sim_num_insn++;
 
-      /* set default reference address and access mode */
-      addr = 0; is_write = FALSE;
+		/* set default reference address and access mode */
+		addr = 0; is_write = FALSE;
 
-      /* set default fault - none */
-      fault = md_fault_none;
+		/* set default fault - none */
+		fault = md_fault_none;
 
-      /* decode the instruction */
-      MD_SET_OPCODE(op, inst);
-      dst_reg = O1;     
- 
-      /* execute the instruction */
-      switch (op)
-	{
+		/* decode the instruction */
+		MD_SET_OPCODE(op, inst);
+                dst_reg = op;     
+
+		/* execute the instruction */
+		switch (op)
+		{
 #define DEFINST(OP,MSK,NAME,OPFORM,RES,FLAGS,O1,O2,I1,I2,I3)				\
-	case OP:									\
-          /*dst_reg = O1;*/                                                 		\
-          new_reg = dst_reg;								\
-          if(isGPR(dst_reg)) {				         			\
-       	     bits_diff = count_bits_different(GPR(prv_reg), GPR(new_reg));       	\
-             g_total_register_operations++;                            		 	\
-             g_total_register_bit_switch += bits_diff;             		    	\
-          }										\
-          SYMCAT(OP,_IMPL);								\
-          break;
+			case OP:									\
+													/*dst_reg = O1;*/                                                 		\
+			new_reg = dst_reg;								\
+			if(isGPR(dst_reg)) {				         			\
+				bits_diff = count_bits_different(GPR(prv_reg), GPR(new_reg));       	\
+				g_total_register_operations++;                            		 	\
+				g_total_register_bit_switch += bits_diff;             		    	\
+			}										\
+			SYMCAT(OP,_IMPL);								\
+			break;
 #define DEFLINK(OP,MSK,NAME,MASK,SHIFT)    						\
-        case OP:									\
-          panic("attempted to execute a linking opcode");
+			case OP:									\
+													panic("attempted to execute a linking opcode");
 #define CONNECT(OP)
 #define DECLARE_FAULT(FAULT)						\
-	  { fault = (FAULT); break; }
+			{ fault = (FAULT); break; }
 #include "machine.def"
-	default:
-	  panic("attempted to execute a bogus opcode");
-      }
+			default:
+				panic("attempted to execute a bogus opcode");
+		}
 
-      if ( MD_OP_FLAGS(op) & F_COND ) g_total_cond_branches++;
-
-
-      if ( MD_OP_FLAGS(op) & F_UNCOND ) g_total_uncond_branches++;
+		if ( MD_OP_FLAGS(op) & F_COND ) g_total_cond_branches++;
 
 
-      if ( MD_OP_FLAGS(op) & F_FCOMP ) g_total_fcomp_branches++;
+		if ( MD_OP_FLAGS(op) & F_UNCOND ) g_total_uncond_branches++;
 
 
-      if ( MD_OP_FLAGS(op) & F_STORE ) g_total_fstore_branches++;
+		if ( MD_OP_FLAGS(op) & F_FCOMP ) g_total_fcomp_branches++;
 
 
-      if ( MD_OP_FLAGS(op) & F_LOAD ) g_total_fload_branches++;
+		if ( MD_OP_FLAGS(op) & F_STORE ) g_total_fstore_branches++;
 
 
-      if ( MD_OP_FLAGS(op) & F_IMM ) g_total_fimm_branches++;
+		if ( MD_OP_FLAGS(op) & F_LOAD ) g_total_fload_branches++;
 
 
-      if (fault != md_fault_none)
-	fatal("fault (%d) detected @ 0x%08p", fault, regs.regs_PC);
+		if ( MD_OP_FLAGS(op) & F_IMM ) g_total_fimm_branches++;
 
-      if (verbose)
-	{
-	  myfprintf(stderr, "%10n [xor: 0x%08x] @ 0x%08p: ",
-		    sim_num_insn, md_xor_regs(&regs), regs.regs_PC);
-	  md_print_insn(inst, regs.regs_PC, stderr);
-	  if (MD_OP_FLAGS(op) & F_MEM)
-	    myfprintf(stderr, "  mem: 0x%08p", addr);
-	  fprintf(stderr, "\n");
-	  /* fflush(stderr); */
+
+		if (fault != md_fault_none)
+			fatal("fault (%d) detected @ 0x%08p", fault, regs.regs_PC);
+
+		if (verbose)
+		{
+			myfprintf(stderr, "%10n [xor: 0x%08x] @ 0x%08p: ",
+					sim_num_insn, md_xor_regs(&regs), regs.regs_PC);
+			md_print_insn(inst, regs.regs_PC, stderr);
+			if (MD_OP_FLAGS(op) & F_MEM)
+				myfprintf(stderr, "  mem: 0x%08p", addr);
+			fprintf(stderr, "\n");
+			/* fflush(stderr); */
+		}
+
+		if (MD_OP_FLAGS(op) & F_MEM)
+		{
+			sim_num_refs++;
+			if (MD_OP_FLAGS(op) & F_STORE)
+				is_write = TRUE;
+		}
+
+
+		/* go to the next instruction */
+		regs.regs_PC = regs.regs_NPC;
+		regs.regs_NPC += sizeof(md_inst_t);
+
+                
+                //printf("%i", regs.regs_PC - regs.regs_TPC);
+              
+
+		/* finish early? */
+		if (max_insts && sim_num_insn >= max_insts){
+			// printf("AVERAGE CHANGE: %f", (float)bytes_diff_counter/(float)reg_change_counter);
+			return;
+		} 
 	}
-
-      if (MD_OP_FLAGS(op) & F_MEM)
-	{
-	  sim_num_refs++;
-	  if (MD_OP_FLAGS(op) & F_STORE)
-	    is_write = TRUE;
-	}
-
-
-      /* go to the next instruction */
-      regs.regs_PC = regs.regs_NPC;
-      regs.regs_NPC += sizeof(md_inst_t);
-
-
-
-      /* finish early? */
-      if (max_insts && sim_num_insn >= max_insts){
-       // printf("AVERAGE CHANGE: %f", (float)bytes_diff_counter/(float)reg_change_counter);
-        return;
-     } 
-    }
 }
 
 
